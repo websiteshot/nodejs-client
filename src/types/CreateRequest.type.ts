1d739237d35d80c587be7348baf348ab764754d2
Adam <i@adamurban.de>
2021-01-02 06:55:55 +0100

    add schedule

--- src/types/CreateRequest.type.ts
+++ src/types/CreateRequest.type.ts
@@ -4,4 +4,9 @@ import { Url } from './Url.type'
 export interface CreateRequest {
   screenshotParameter: ScreenshotParameter
   urls: Url[]
+  scheduledTs?: number
+  scheduleDescription?: {
+    value: number
+    unit: 'm' | 'h' | 'd'
+  }
 }
