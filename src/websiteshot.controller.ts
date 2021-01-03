726e8f907c1bd00adc7d9e5f153ab034255ee98c
Adam <i@adamurban.de>
2021-01-03 06:22:50 +0100

    add response

--- src/websiteshot.controller.ts
+++ src/websiteshot.controller.ts
@@ -1,13 +1,14 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { Config } from './types/Config.type'
 import { CreateRequest } from './types/CreateRequest.type'
+import { CreateResponse } from './types/CreateResponse.type'
 
 const BASE_URL = `https://api.websiteshot.app`
 
 export class WebsiteshotController {
   constructor(private config: Config) {}
 
-  public async create(request: CreateRequest) {
+  public async create(request: CreateRequest): Promise<CreateResponse> {
     const url = `${BASE_URL}/api/projects/${this.config.projectId}`
     const data = {
       screenshotParameter: request.screenshotParameter,
@@ -24,7 +25,8 @@ export class WebsiteshotController {
     }
 
     try {
-      await axios(config)
+      const result: AxiosResponse = await axios(config)
+      return result ? result.data : undefined
     } catch (error) {
       throw new Error(`Failed to create Screenshot Job: ${error.message}`)
     }
